ec117870dd68f7683a5cee0be2c4ba06697be1ab
kmlee95 <kmlee95@naver.com>
2021-08-10 22:19:12 +0900

    [front #1] chore : _document, _app, emotion 적용

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,12 +1,21 @@
 import type { AppProps } from 'next/app';
-import { globalStyles } from '@theme/globalStyle';
+import { ThemeProvider } from '@emotion/react';
+import Head from 'next/head';
 
+import { globalStyles } from '@theme/globalStyle';
+import theme from '@theme/.';
+
+//최초로 실행되며, 이곳에서 렌더링 하는 겂은 모든페이지에 영향
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
+      <Head>
+        <title>Nextjs App with TypeScript</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       {globalStyles}
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   );
 }
 export default MyApp;
