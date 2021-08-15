4eec6a56952587d99043fb0ca8c8057254d8876a
HwanChang <hwanchang.dev@gmail.com>
2021-08-16 03:21:47 +0900

    [back #5] init: back-end 서버 초기 구성

---
+++ server/src/main/java/com/babmukja/server/Application.java
@@ -0,0 +1,15 @@
+package com.babmukja.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jdbc.repository.config.EnableJdbcAuditing;
+
+@EnableJdbcAuditing
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
